4dae1170eb958a88c93fe8127dcbe91182466fcb
Hai Nguyen <hnguye28@umd.edu>
2020-12-05 13:37:03 -0500

    fix delete error and delete alarm works

--- app/src/main/java/com/example/callyourmother/ContactDetails.kt
+++ app/src/main/java/com/example/callyourmother/ContactDetails.kt
@@ -61,6 +61,12 @@ class ContactDetails {
         lastCalled = update
     }
 
+    fun getUniqueID() : Int{
+        val unformattedPhoneNumber = phoneNumber?.replace("\\D".toRegex(),"" )
+        return if (unformattedPhoneNumber != null) {
+            unformattedPhoneNumber.toLong().toInt()
+        } else 0
+    }
 
     companion object{
 
